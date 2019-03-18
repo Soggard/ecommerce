120c5681bd63a51c5fe5c2bff096ca2464b1967c
Soggard <e.bernet@laposte.net>
2019-03-18 16:49:33 +0100

    Administration

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 
 
 #Environnement
-Pour lancer le serveur :
+#####Pour lancer le serveur :
 >php bin/console server:run
 
-Pour démarrer la base de données après avoir lancé Docker
+#####Pour démarrer la base de données après avoir lancé Docker
 >docker build .
-> docker-compose up
+
+>docker-compose up
+
+#####Pour initialiser la base de données
+>php bin/console doctrine:database:create 
+
+>php bin/console doctrine:schema:update --force
+
+#Commandes
+
+##### Créer une entité
+>php bin/console make:entity
+
+##### Exécuter les fixtures load
+>php bin/console make:fixtures
+
+>php bin/console doctrine:fixtures:load
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->encoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $product = new Product();
+            $product->setName($i);
+            $product->setPrice(rand(10, 50));
+            $product->setDescription("Description du produit ".$i);
+
+            $manager->persist($product);
+        }
+
+        $user = new User();
+        $user->setEmail("e.bernet@laposte.net");
+        $user->setRoles([
+            'ROLE_ADMIN'
+        ]);
+        $user->setPassword($this->encoder->encodePassword($user, 'password'));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
