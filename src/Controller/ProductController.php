5c192a3b8138a657b73d20a03ce11c4586de713f
Soggard <e.bernet@laposte.net>
2019-03-19 09:52:41 +0100

    Product API

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\Common\Persistence\ObjectManager;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as FOSRest;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends FOSRestController
+{
+    /**
+     * @FOSRest\Get("/api/products")
+     *
+     * @param ObjectManager $manager
+     *
+     * @return Response
+     */
+    public function getProductsAction(ObjectManager $manager)
+    {
+        $productRepository = $manager->getRepository(Product::class);
+        $products = $productRepository->findAll();
+
+        return $this->json($products, Response::HTTP_OK);
+    }
+}
